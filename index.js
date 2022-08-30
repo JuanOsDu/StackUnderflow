51e3a0a70a755d208185ee693cb92245a9aebab8
Juan <juan0806@outlook.es>
2022-08-30 11:16:48 -0500

    test deploy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-app.use('/v1/api', require('./routes/country.routes'));
+//app.use('/v1/api', require('./routes/country.routes'));
 app.use('/v1/api', require('./routes/user.routes'));
 app.listen(port, () => {
     console.log("Online on "+port);
